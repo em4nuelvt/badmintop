911818d79bec0e386bf0f997a5051caabf501b44
Emanuel Vieira Tavares <emanuelvieiratavaresvt@gmail.com>
2020-09-28 09:45:32 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'homePage.dart';
+
+void main (){
+  runApp(MaterialApp(
+    title: "badmintop",
+    home: Home()
+  ));
+}
+
+
+
+
+
---
+++ lib/pratic.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+ class praticPage extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+         appBar: AppBar(
+           title: Text("BadTop (praticante)"),
+           centerTitle: true,
+           actions: <Widget>[
+             Icon(Icons.account_circle, size: 40.0,)
+           ],
+
+         ),
+         drawer: Drawer(
+           child: ListView(
+             padding: EdgeInsets.zero,
+             children: <Widget>[
+               UserAccountsDrawerHeader(
+                 accountName: Text("Professor"),
+                 accountEmail: Text("professor@professor.com"),
+                 currentAccountPicture: CircleAvatar(
+                   radius: 30.0,
+                   backgroundColor: Colors.white,
+                 ),
+               ),
+               ListTile(
+                 title: Text('Configurações',
+                   style: TextStyle(color: Colors.blue),),
+                 onTap: () {
+                   Navigator.pop(context);
+                 },
+               ),
+               ListTile(
+                 title: Text('Criar Plano de Aula',
+                     style: TextStyle(color: Colors.blue)),
+                 onTap: () {
+                   Navigator.pop(context);
+                 },
+               ),
+             ],
+           ),
+         ),
+         body: Padding(
+           padding: EdgeInsets.all(7.0),
+           child: Column(
+             crossAxisAlignment: CrossAxisAlignment.stretch,
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: <Widget>[
+               Container(
+                 height: 70.0,
+                 child: RaisedButton(
+                   child: Text("Como jogar Badminton",
+                       style: TextStyle(fontSize: 20.0, color: Colors.white)),
+                   color: Colors.blue,
+                   onPressed: (){},
+                 ),
+               ),
+               Divider(
+                 height: 45.0,
+               ),
+               Container(
+                 height: 70.0,
+                 child: RaisedButton(
+                   child: Text("Equipamentos",
+                       style: TextStyle(fontSize: 20.0, color: Colors.white)),
+                   color: Colors.blue,
+                   onPressed: (){},
+                 ),
+               ),
+               Divider(
+                 height: 45.0,
+               ),
+               Container(
+                 height: 70.0,
+                 child: RaisedButton(
+                   child: Text("Regras do Badminton",
+                       style: TextStyle(fontSize: 20.0, color: Colors.white)),
+                   color: Colors.blue,
+                   onPressed: (){},
+                 ),
+               ),
+               Divider(
+                 height: 45.0,
+               ),
+               Container(
+                 height: 70.0,
+                 child: RaisedButton(
+                   child: Text("História",
+                       style: TextStyle(fontSize: 20.0, color: Colors.white)),
+                   color: Colors.blue,
+                   onPressed: (){},
+                 ),
+               ),
+
+             ],
+           ),
+         )
+     );
+   }
+ }
